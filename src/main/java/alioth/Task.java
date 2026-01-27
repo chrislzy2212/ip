b3caacc6838db5b9e3284a0aa2b6857c625f183f
Christopher Lu Zi Yang <luziyang2212@gmail.com>
2026-01-28 01:53:06 +0800

    Add Task class

---
+++ src/main/java/alioth/Task.java
@@ -0,0 +1,40 @@
+package alioth;
+
+/**
+ * Represents a task with a description and a completion status.
+ */
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    /**
+     * Creates a new task with the given description.
+     * The task is not done by default.
+     *
+     * @param description Description of the task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Sets the completion status of this task.
+     *
+     * @param status True to mark as done, false to mark as not done.
+     */
+    public void setDone(boolean status) {
+        isDone = status;
+    }
+
+    /**
+     * Returns a string representation of this task.
+     *
+     * @return Task in the form "[X] description" or "[ ] description".
+     */
+    @Override
+    public String toString() {
+        String status = isDone ? "X" : " ";
+        return "[" + status + "] " + description;
+    }
+}
