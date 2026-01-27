a98cbc5a267a2edb5671d30d33ac56ddc078517e
Christopher Lu Zi Yang <luziyang2212@gmail.com>
2026-01-28 02:14:32 +0800

    Add mark and unmark commands

--- src/main/java/alioth/Alioth.java
+++ src/main/java/alioth/Alioth.java
@@ -9,7 +9,7 @@ public class Alioth {
     private static final String LINE = "____________________________________________________________";
 
     private static final int MAX_TASKS = 100;
-    private static final String[] TASKS = new String[MAX_TASKS];
+    private static final Task[] TASKS = new Task[MAX_TASKS];
     private static int taskCount = 0;
 
     /**
@@ -30,14 +30,49 @@ public class Alioth {
                 break;
             } else if (input.equals("list")) {
                 printList();
+            } else if (input.startsWith("mark ")) {
+                markTask(input);
+            } else if (input.startsWith("unmark ")) {
+                unmarkTask(input);
             } else {
                 addTask(input);
             }
         }
     }
 
+    private static int parseTaskNumber(String input) {
+        String[] parts = input.split(" ", 2);
+        return Integer.parseInt(parts[1]);
+    }
+
+    private static void markTask(String input) {
+        int taskNumber = parseTaskNumber(input);
+        int index = taskNumber - 1;
+
+        Task task = TASKS[index];
+        task.setDone(true);
+
+        System.out.println(LINE);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + task);
+        System.out.println(LINE);
+    }
+
+    private static void unmarkTask(String input) {
+        int taskNumber = parseTaskNumber(input);
+        int index = taskNumber - 1;
+
+        Task task = TASKS[index];
+        task.setDone(false);
+
+        System.out.println(LINE);
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  " + task);
+        System.out.println(LINE);
+    }
+
     private static void addTask(String task) {
-        TASKS[taskCount] = task;
+        TASKS[taskCount] = new Task(task);
         taskCount++;
 
         System.out.println(LINE);
@@ -47,9 +82,10 @@ public class Alioth {
 
     private static void printList() {
         System.out.println(LINE);
+        System.out.println("Here are the tasks in your list:");
 
         for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + TASKS[i]);
+            System.out.println((i + 1) + "." + TASKS[i]);
         }
 
         System.out.println(LINE);
