fd9678e68c3416575f9cb8042f99cfed5b1e1a69
Christopher Lu Zi Yang <luziyang2212@gmail.com>
2026-01-31 23:33:05 +0800

    Use LocalDate for Deadline

--- src/main/java/alioth/Deadline.java
+++ src/main/java/alioth/Deadline.java
@@ -1,33 +1,39 @@
 package alioth;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
- * Represents a task that needs to be done before a specific date/time.
+ * Represents a task that needs to be done before a specific date.
  */
 public class Deadline extends Task {
-    private final String by;
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy");
+
+    private final LocalDate by;
 
     /**
-     * Creates a deadline task with the given description and deadline.
+     * Creates a deadline task with the given description and deadline date.
      *
      * @param description Description of the task.
-     * @param by Deadline of the task.
+     * @param by Deadline date of the task.
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
     /**
-     * Returns the deadline time of this task.
+     * Returns the deadline date of this task.
      *
-     * @return Deadline time.
+     * @return Deadline date.
      */
-    public String getBy() {
+    public LocalDate getBy() {
         return by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 }
